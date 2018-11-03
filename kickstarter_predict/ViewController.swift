bf16dea91b4cd5473f5783d3b9938f1571fa26f3
刘祥 <xiangliu@s-macbook-pro-3.local>
2018-11-03 18:33:36 -0400

    Initial Commit

---
+++ kickstarter_predict/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kickstarter_predict
+//
+//  Created by 刘祥 on 11/3/18.
+//  Copyright © 2018 shaneliu90. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
